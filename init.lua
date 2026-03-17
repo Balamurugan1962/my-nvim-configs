754516112b43474260b533e65c9bc08a92299d35
Balamurugan S <balamurugan.sekar@icloud.com>
2026-03-17 21:36:48 +0530

    basic options

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
-print("Here we go again.")
+require('options')
+
---
+++ lua/options.lua
@@ -0,0 +1,12 @@
+-- line number
+vim.opt.number = true
+
+-- highlight the cursor line
+vim.opt.cursorline = true
+
+-- relative number
+vim.opt.relativenumber = true
+
+-- tab spaces
+vim.opt.shiftwidth = 4
+
