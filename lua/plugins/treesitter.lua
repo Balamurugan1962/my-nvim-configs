2bec38d389f05da8ad95c4021f6927f161ef29e6
Balamurugan S <balamurugan.sekar@icloud.com>
2026-03-19 11:17:26 +0530

    treesitter add aditional laguages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,27 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+
+  event = { "BufReadPost", "BufNewFile" }, -- loads treesitter only when a file opens
+
+  config = function()
+    local ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not ok then
+      return
+    end
+
+    configs.setup({
+      ensure_installed = {
+        "c", "cpp", "rust", "lua", "python"
+      },
+
+      highlight = {
+        enable = true,
+      },
+
+      indent = {
+        enable = true,
+      },
+    })
+  end
 }
